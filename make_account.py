ffa2ed25003664ee10a4adc9615e8d6410da25e0
Trevor <TrevorKirkby123@gmail.com>
2014-05-20 09:25:53 -0700

    deleted useless file

--- make_account.py
+++
@@ -1,9 +0,0 @@
-from network import *
-
-conn = socket(1340)
-conn.connect(raw_input("Please enter the server host: "))
-conn.send("new_user")
-
-conn.send(raw_input("enter username: "))
-conn.send(raw_input("enter password: "))
-conn.send(raw_input("enter email: "))
